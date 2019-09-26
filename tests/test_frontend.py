a7630102ffee216aa337b89f92c8c6c63691cfaa
henry <henrybochers@yahoo.com>
2019-09-26 10:32:16 -0500

    add test for items web view

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -1,29 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import pytest
 import tyko
 import tyko.database
 
-
-def test_view_web_object_empty():
+@pytest.fixture()
+def app():
     app = Flask(__name__, template_folder="../tyko/templates")
     db = SQLAlchemy(app)
     tyko.create_app(app)
     tyko.database.init_database(db.engine)
     app.config["TESTING"] = True
+    return app
 
+
+def test_view_web_object_empty(app):
     with app.test_client() as server:
         resulting_webpage = server.get("/object/1")
         assert resulting_webpage.status_code != 200
 
 
-def test_view_web_object():
-    app = Flask(__name__, template_folder="../tyko/templates")
-    db = SQLAlchemy(app)
-    tyko.create_app(app)
-    tyko.database.init_database(db.engine)
-    app.config["TESTING"] = True
-
+def test_view_web_object(app):
     with app.test_client() as server:
         server.post("/api/object/",
                     data={
@@ -34,3 +31,18 @@ def test_view_web_object():
         assert resulting_webpage.status_code == 200
         data = str(resulting_webpage.data, encoding="utf-8")
         assert "<td>my stupid object</td>" in data
+
+
+def test_view_web_item(app):
+    with app.test_client() as server:
+        server.post("/api/item/",
+                    data={
+                        "name": "My dummy item",
+                        "file_name": "dummy.txt",
+                        "medusa_uuid": "03de08f0-dada-0136-5326-0050569601ca-4"
+                    }
+                )
+        resulting_webpage = server.get("/item/1")
+        assert resulting_webpage.status_code == 200
+        data = str(resulting_webpage.data, encoding="utf-8")
+        assert "<td>My dummy item</td>" in data
