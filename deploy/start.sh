74ae586f226ef7d6c9516a56e0b2fe9873990c0c
henry <henrybochers@yahoo.com>
2019-09-30 16:34:32 -0500

    don't automate init database

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-python -m tyko init-db
+#echo "Initalizing database"
+#python -m tyko init-db
 echo "Starting Tyko"
 gunicorn --workers=5  --bind 0.0.0.0:8000  "tyko.run:create_app()"
