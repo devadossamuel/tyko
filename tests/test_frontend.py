af6ed70ed8c2c15e693ffd932730db40d20d5f9a
Henry Borchers <hborcher@illinois.edu>
2020-03-05 13:36:07 -0600

    projects, objects, and items details have breadcrumbs

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -3,11 +3,12 @@ import sys
 
 from pyexpat import ExpatError
 
-from flask import Flask
+from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pytest
 import tyko
 import tyko.database
+from tyko import frontend
 from xml.dom.minidom import parseString
 
 
@@ -29,41 +30,117 @@ def test_view_web_object_empty(app):
 
 def test_view_web_object(app):
     with app.test_client() as server:
-        server.post(
-            "/api/object/",
+        project_id = server.post(
+            "/api/project/",
+            data=json.dumps(
+                {
+                    "title": "my dumb project",
+                }
+            ),
+            content_type='application/json'
+        ).get_json()['id']
+        object_id = server.post(
+            url_for("project_add_object", project_id=project_id),
             data=json.dumps(
                 {
                     "name": "my stupid object",
                 }
             ),
             content_type='application/json'
+        ).get_json()['object']['object_id']
+        # page_project_object_details
+        resulting_webpage = server.get(
+            url_for(
+                "page_project_object_details",
+                project_id=project_id,
+                object_id=object_id
+            )
         )
-        resulting_webpage = server.get("/object/1")
         assert resulting_webpage.status_code == 200
-        data = str(resulting_webpage.data, encoding="utf-8")
-        # try:
-        #     html_page = parseString(data)
-        # except ExpatError as e:
-        #     print("Error with {}".format(data.splitlines()[e.lineno-2:e.lineno]), file=sys.stderr)
-        #     raise
+        data = str(
+            resulting_webpage.data, encoding="utf-8")
         assert "my stupid object" in data
 
 
 def test_view_web_item(app):
     with app.test_client() as server:
-        server.post("/api/item/",
-                    data=json.dumps(
-                        {
-                            "name": "My dummy item",
-                            "file_name": "dummy.txt",
-                            "medusa_uuid": "03de08f0-dada-0136-5326-0050569601ca-4",
-                            "format_id": 1
-                        }
-                    ),
-                    content_type='application/json'
+        project_id = server.post(
+            "/api/project/",
+            data=json.dumps(
+                {
+                    "title": "my dumb project",
+                }
+            ),
+            content_type='application/json'
+        ).get_json()['id']
 
-                )
-        resulting_webpage = server.get("/item/1")
+        object_id = server.post(
+            url_for("project_add_object", project_id=project_id),
+            data=json.dumps(
+                {
+                    "name": "my stupid object",
+                }
+            ),
+            content_type='application/json'
+        ).get_json()['object']['object_id']
+        new_item_rep = server.post(
+            url_for(
+                'project_object_add_item',
+                project_id=project_id,
+                object_id=object_id
+            ),
+            data=json.dumps(
+                {
+                    "name": "My dummy item",
+                    "file_name": "dummy.txt",
+                    "medusa_uuid": "03de08f0-dada-0136-5326-0050569601ca-4",
+                    "format_id": 1
+                }
+            ),
+            content_type='application/json'
+
+        )
+        assert new_item_rep.status_code == 200
+        new_item_data = new_item_rep.get_json()['item']
+        resulting_webpage = server.get(
+            url_for(
+                "page_project_object_item_details",
+                project_id=project_id,
+                object_id=object_id,
+                item_id=new_item_data['item_id'])
+        )
         assert resulting_webpage.status_code == 200
         data = str(resulting_webpage.data, encoding="utf-8")
         assert "My dummy item" in data
+
+
+@pytest.fixture()
+def breadcrumb_builder_with_project():
+    builder = frontend.BreadcrumbBuilder()
+    builder["Project"] = "http://127.0.0.1:5000/project/1"
+    return builder
+
+
+def test_breadcrumb_builder_len(breadcrumb_builder_with_project):
+    assert len(breadcrumb_builder_with_project) == 1
+
+
+def test_breadcrumb_builder_get_key(breadcrumb_builder_with_project):
+    assert breadcrumb_builder_with_project["Project"] == "http://127.0.0.1:5000/project/1"
+
+
+def test_breadcrumb_builder_iter(breadcrumb_builder_with_project):
+    for v in breadcrumb_builder_with_project:
+        assert v == ("Project", "http://127.0.0.1:5000/project/1")
+
+
+def test_breadcrumb_builder_builds(breadcrumb_builder_with_project):
+    breadcrumbs = breadcrumb_builder_with_project.build(active_level="Project")
+    assert isinstance(breadcrumbs, list)
+    assert len(breadcrumbs) == 1
+
+
+def test_breadcrumb_builder_del(breadcrumb_builder_with_project):
+    assert len(breadcrumb_builder_with_project) == 1
+    del breadcrumb_builder_with_project["Project"]
+    assert len(breadcrumb_builder_with_project) == 0
--- tyko/frontend.py
+++ tyko/frontend.py
@@ -1,7 +1,10 @@
 # pylint: disable=invalid-name
 
 import abc
-from typing import Tuple, Set, Optional, Callable, List
+import collections.abc
+from abc import ABC
+from typing import Tuple, Set, Optional, Callable, List, Iterator, \
+    NamedTuple, Dict
 from dataclasses import dataclass
 
 from flask import make_response, render_template, url_for
@@ -26,6 +29,58 @@ class Details:
     source_key: Optional[Callable[[], str]] = None
     key_branch: Optional[str] = None
     editable: bool = False
+
+
+class Breadcrumb(NamedTuple):
+    name: str
+    link: str
+    current: bool
+
+
+class BreadcrumbBuilder(collections.abc.MutableMapping):
+    VALID_CRUMBS = [
+        "Project",
+        "Object",
+        "Item"
+    ]
+
+    def __init__(self) -> None:
+        self._data: Dict[str, str] = dict()
+
+    def __delitem__(self, v: str) -> None:
+        del self._data[v]
+
+    def __getitem__(self, k: str) -> str:
+        return self._data[k]
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __iter__(self) -> Iterator[Tuple[str, str]]:
+        for k in BreadcrumbBuilder.VALID_CRUMBS:
+            if k in self._data:
+                yield k, self._data[k]
+
+    def __setitem__(self, k: str, v: str) -> None:
+        if k not in BreadcrumbBuilder.VALID_CRUMBS:
+            raise ValueError("Not a valid breadcrumb level")
+        self._data[k] = v
+
+    def build(self, active_level) -> List[Breadcrumb]:
+        if active_level not in BreadcrumbBuilder.VALID_CRUMBS:
+            raise ValueError("Not a valid breadcrumb type")
+
+        breadcrumbs: List[Breadcrumb] = list()
+        for k in BreadcrumbBuilder.VALID_CRUMBS:
+            self._add_crumb(breadcrumbs, k, current=active_level == k)
+
+        return breadcrumbs
+
+    def _add_crumb(self, breadcrumbs: List[Breadcrumb], key, current) -> None:
+
+        if key in self._data:
+            breadcrumbs.append(
+                Breadcrumb(name=key, link=self._data[key], current=current))
 
 
 class AbsFrontend(metaclass=abc.ABCMeta):
@@ -165,7 +220,26 @@ class FrontendEditable(FrontendEntity):
         """Generate the page for the edit an existing entity"""
 
 
-class ProjectFrontend(FrontendEditable):
+class ProjectComponentDetailFrontend(FrontendEntity, ABC):  # noqa: E501 pylint: disable=W0223
+    @staticmethod
+    def build_breadcrumbs(active_level, project_url=None, object_url=None,
+                          item_url=None) -> List[Breadcrumb]:
+
+        breadcrumb_builder = BreadcrumbBuilder()
+
+        if project_url is not None:
+            breadcrumb_builder['Project'] = project_url
+
+        if object_url is not None:
+            breadcrumb_builder['Object'] = object_url
+
+        if item_url is not None:
+            breadcrumb_builder['Item'] = item_url
+
+        return breadcrumb_builder.build(active_level)
+
+
+class ProjectFrontend(ProjectComponentDetailFrontend):
 
     def __init__(self, provider: data_provider.DataProvider) -> None:
         super().__init__(provider)
@@ -212,9 +286,6 @@ class ProjectFrontend(FrontendEditable):
         for note_type in self._data_connector.get_note_types():
             valid_note_types.append((note_type.name, note_type.id))
 
-        collections = data_provider.CollectionDataConnector(
-            self._data_connector.session_maker).get(serialize=True)
-
         return self.render_page(
             template="project_details.html",
             project=selected_project,
@@ -223,7 +294,15 @@ class ProjectFrontend(FrontendEditable):
             project_id=entity_id,
             valid_note_types=valid_note_types,
             fields=fields,
-            collections=collections
+            breadcrumbs=self.build_breadcrumbs(
+                active_level="Project",
+                project_url=url_for(
+                    "page_project_details",
+                    project_id=entity_id
+                )
+            ),
+            collections=data_provider.CollectionDataConnector(
+                self._data_connector.session_maker).get(serialize=True)
             )
 
     def edit_details(self, entity_id):
@@ -251,7 +330,7 @@ class ProjectFrontend(FrontendEditable):
         return super().render_page(template, **context)
 
 
-class ItemFrontend(FrontendEntity):
+class ItemFrontend(ProjectComponentDetailFrontend):
     def __init__(self, provider: data_provider.DataProvider) -> None:
         super().__init__(provider)
 
@@ -290,16 +369,30 @@ class ItemFrontend(FrontendEntity):
             if f.source_key is not None:
                 selected_item[f.key] = f.source_key()
 
+        breadcrumbs = self.build_breadcrumbs(
+            active_level="Item",
+            project_url=url_for(
+                "page_project_details",
+                project_id=kwargs["project_id"]),
+            item_url="",
+            object_url=url_for(
+                "page_object_details",
+                object_id=kwargs['object_id'])
+        )
+
         valid_note_types = []
         for note_type in self._data_connector.get_note_types():
             valid_note_types.append((note_type.name, note_type.id))
-        return self.render_page(template="item_details.html",
-                                project_id=kwargs.get("project_id"),
-                                valid_note_types=valid_note_types,
-                                object_id=kwargs.get("object_id"),
-                                fields=fields,
-                                api_path=api_path,
-                                item=selected_item)
+
+        return self.render_page(
+            template="item_details.html",
+            project_id=kwargs.get("project_id"),
+            valid_note_types=valid_note_types,
+            object_id=kwargs.get("object_id"),
+            fields=fields,
+            breadcrumbs=breadcrumbs,
+            api_path=api_path,
+            item=selected_item)
 
     @property
     def entity_title(self) -> str:
@@ -314,7 +407,7 @@ class ItemFrontend(FrontendEntity):
         return "page_item"
 
 
-class ObjectFrontend(FrontendEditable):
+class ObjectFrontend(ProjectComponentDetailFrontend):
     def __init__(self, provider: data_provider.DataProvider) -> None:
         super().__init__(provider)
         self._data_provider = provider
@@ -376,7 +469,6 @@ class ObjectFrontend(FrontendEditable):
 
         selected_object = self._data_connector.get(serialize=True,
                                                    id=entity_id)
-        api_path = f"{url_for('.page_index')}api/object/{entity_id}"
 
         collection = selected_object.get('collection')
         if collection is not None:
@@ -399,6 +491,7 @@ class ObjectFrontend(FrontendEditable):
             valid_note_types.append((note_type.name, note_type.id))
 
         project = selected_object.get('project')
+
         if project is not None:
             project_name = project['title']
             project_id = project['project_id']
@@ -410,8 +503,20 @@ class ObjectFrontend(FrontendEditable):
             edit=False,
             fields=fields,
             formats=self._data_provider.get_formats(serialize=True),
-            api_path=api_path,
+            api_path=url_for('object_by_id', id=entity_id),
             valid_note_types=valid_note_types,
+            breadcrumbs=self.build_breadcrumbs(
+                "Object",
+                project_url=url_for(
+                    "page_project_details",
+                    project_id=project['project_id']
+                ),
+                object_url=url_for(
+                    "page_project_object_details",
+                    project_id=project['project_id'],
+                    object_id=selected_object['object_id']
+                )
+            ),
             object=selected_object)
 
     def render_page(self, template="object_details.html", **context):
