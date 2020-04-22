282d5b7ac370ec51ff85671b85c70a7508e841ac
henry <henryborchers@yahoo.com>
2020-04-22 16:49:34 -0500

    use google style guidelines for js

--- tyko/static/js/api.js
+++ tyko/static/js/api.js
@@ -1,21 +1,21 @@
-import {requests} from "./request.js"
+import {requests} from './request.js';
 
 function addNote(apiRoute, noteTypeId, text) {
-  if(apiRoute === undefined){
-    throw new TypeError("apiRoute is a required field")
+  if (apiRoute === undefined) {
+    throw new TypeError('apiRoute is a required field');
   }
-  if(noteTypeId === undefined){
-    throw new TypeError("noteTypeId is a required field")
+  if (noteTypeId === undefined) {
+    throw new TypeError('noteTypeId is a required field');
   }
-  if(text === undefined){
-    throw new TypeError("text is a required field")
+  if (text === undefined) {
+    throw new TypeError('text is a required field');
   }
 
-  const newNoteRoute = apiRoute + "/notes";
+  const newNoteRoute = apiRoute + '/notes';
 
   const data = {
-    "text": text,
-    "note_type_id": noteTypeId
+    'text': text,
+    'note_type_id': noteTypeId,
   };
   return requests.post(newNoteRoute, data);
 }
@@ -25,105 +25,106 @@ const getNotes = () => {
   return requests.get(apiPath);
 };
 
-function editNote(apiRoute, noteTypeId, text){
-  if(apiRoute === undefined){
-    throw("apiRoute is a required field")
+function editNote(apiRoute, noteTypeId, text) {
+  if (apiRoute === undefined) {
+    throw('apiRoute is a required field');
   }
-  if(noteTypeId === undefined){
-    throw("noteTypeId is a required field")
+  if (noteTypeId === undefined) {
+    throw('noteTypeId is a required field');
   }
-  if(text === undefined){
-    throw("text is a required field")
+  if (text === undefined) {
+    throw('text is a required field');
   }
 
   const data = {
-    "text": text,
-    "note_type_id": noteTypeId
+    'text': text,
+    'note_type_id': noteTypeId,
   };
 
   return requests.put(apiRoute, data);
 }
 
 function removeNote(apiRoute) {
-  if(apiRoute === undefined){
-    throw("apiRoute is a required field")
+  if (apiRoute === undefined) {
+    throw('apiRoute is a required field');
   }
-  return requests.delete(apiRoute)
+  return requests.delete(apiRoute);
 }
 
 export const notes = {
-  "getNotes": getNotes,
-  "addNote": addNote,
-  "editNote": editNote,
-  "removeNote": removeNote
+  'getNotes': getNotes,
+  'addNote': addNote,
+  'editNote': editNote,
+  'removeNote': removeNote,
 };
 
-
 function addObject(apiRoute, data) {
-  if(apiRoute === undefined){
-    throw("apiRoute is a required field")
+  if (apiRoute === undefined) {
+    throw('apiRoute is a required field');
   }
   return requests.post(apiRoute, data);
 }
 
-function removeObject(apiRoute){
-  if(apiRoute === undefined){
-    throw("apiRoute is a required field")
+function removeObject(apiRoute) {
+  if (apiRoute === undefined) {
+    throw('apiRoute is a required field');
   }
-  return requests.delete(apiRoute)
+  return requests.delete(apiRoute);
 }
 
 export const objects = {
-  "addObject": addObject,
-  "removeObject": removeObject
+  'addObject': addObject,
+  'removeObject': removeObject,
 };
 
 function addItem(apiRoute, data) {
-  if(apiRoute === undefined){
-    throw("apiRoute is a required field")
+  if (apiRoute === undefined) {
+    throw('apiRoute is a required field');
   }
   return requests.post(apiRoute, data);
 }
+
 function editFileNote(apiRoute, data) {
   return requests.put(apiRoute, data);
 }
+
 function addFileNote(apiRoute, data) {
   return requests.post(apiRoute, data);
 }
+
 function addFile(apiRoute, data) {
-  return requests.post(apiRoute, data)
+  return requests.post(apiRoute, data);
 }
 
 export const items = {
-  "addItem": addItem,
-  "addFile": addFile
+  'addItem': addItem,
+  'addFile': addFile,
 };
 
-
 export const collections = {
-  "new": requests.post
+  'new': requests.post,
 };
 
 export const files = {
-  "addFile": addFile,
-  "removeFile": (apiRoute)=>{
+  'addFile': addFile,
+  'removeFile': (apiRoute) => {
     return requests.delete(apiRoute);
   },
-  "removeNote": (apiRoute)=>{
+  'removeNote': (apiRoute) => {
     return requests.delete(apiRoute);
   },
-  "addNote": addFileNote,
-  "editNote": editFileNote
-}
+  'addNote': addFileNote,
+  'editNote': editFileNote,
+};
 export const routes = {
-  "all_routes": (baseURL)=> {
-    return requests.get(baseURL).then(function (result) {
-        let d = JSON.parse(result)
+  'all_routes': (baseURL) => {
+    return requests.get(baseURL).then(function(result) {
+      let d = JSON.parse(result);
       let dataRoutes = {};
-      for (let i =0; i < d.length; i++){
+      for (let i = 0; i < d.length; i++) {
         dataRoutes[d[i].endpoint.toString()] = d[i];
       }
-        return dataRoutes;
+      return dataRoutes;
     });
-  }
-}
+  },
+};
