99de4ad5cfac72e9d89e84170507d250e7e47748
henry <henryborchers@yahoo.com>
2020-04-23 11:11:06 -0500

    removed unused param and added jsdoc comments

--- tyko/static/js/noteModal.js
+++ tyko/static/js/noteModal.js
@@ -1,8 +1,20 @@
+/**
+ * Initializes a modal dialog
+ * @param {jQuery} modal - modal used as the dialog box
+ * @param {jQuery} button - Button used
+ */
 export function setupModalWindow(modal, button) {
   modal.find('.modal-title').text(button.data('title'));
   modal.find('#message-text').val(button.data('text'));
 }
 
+/**
+ * Handle the result of editing a note to the modal dialog box.
+ * @param {jQuery} button - Button used
+ * @param {jQuery} modal - modal used as the dialog box
+ * @param {Promise | Promise<Object>} promise - resulting promise that made a
+ * request made to server
+ */
 export function editNote(button, modal, promise) {
   const noteId = button.data('notetype');
   modal.find('#noteTypeSelect').val(noteId);
@@ -19,7 +31,14 @@ export function editNote(button, modal, promise) {
       });
 }
 
-export function newNote(button, modal, apiPath, promise) {
+/**
+ * Handle the result of a note creation to the modal dialog box.
+ * @param {jQuery} modal - modal used as the dialog box
+ * @param {string} apiPath - url to request a note be added
+ * @param {Promise | Promise<Object>} promise - resulting promise that made a
+ * request made to server
+ */
+export function newNote(modal, apiPath, promise) {
   modal.find('#saveNoteButton').unbind('click').bind('click',
       function() {
         const noteTypeId = modal.find('#noteTypeSelect').val();
@@ -32,7 +51,14 @@ export function newNote(button, modal, apiPath, promise) {
       });
 }
 
-export function removeNote(button, modal, apiroute, promise) {
+/**
+ * Handle the result of a note removal to the modal dialog box.
+ * @param {jQuery} modal - modal used as the dialog box
+ * @param {string} apiroute - url to request a note be added
+ * @param {Promise | Promise<Object>} promise - resulting promise that made a
+ * request made to server
+ */
+export function removeNote(modal, apiroute, promise) {
   modal.find('#confirmRemoveNoteButton').unbind('click').bind('click',
       function() {
         promise(apiroute).then(function() {
