4cf611028164c5a4239c5a1f1461494c47c48941
henry <henryborchers@yahoo.com>
2020-05-07 17:43:32 -0500

    whitespace

--- tyko/static/js/load_format_options.mjs
+++ tyko/static/js/load_format_options.mjs
@@ -47,27 +47,27 @@ $('.tyko-form-new-entity').each(function() {
               data[key[0]] = o;
             }
           }
-
         }
         items.addItem(addUrl, data).then(function() {
           location.reload();
         }).catch(function(reason) {
           const alertBox = $('#submitResultAlert');
           let responsesMessage =
-              '<div class="alert alert-danger alert-dismissible" role="alert" id="submitResultAlert">\n' +
+              '<div class="alert alert-danger alert-dismissible" ' +
+              'role="alert" id="submitResultAlert">\n' +
               '<strong id="errorMessage">';
 
           responsesMessage += reason.statusText;
           responsesMessage += '</strong>';
           responsesMessage +=
-              '    <button type="button" class="close" data-dismiss="alert" aria-label="Close">\n' +
+              '    <button type="button" class="close" data-dismiss="alert" ' +
+              'aria-label="Close">\n' +
               '        <span aria-hidden="true">&times;</span>\n' +
               '    </button>\n' +
               '</div>';
           alertBox.html(responsesMessage);
           console.error(reason.responseText);
-          },
-        );
+        });
         return false;
       },
   );
