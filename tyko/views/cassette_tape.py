1ed8297be0adcbb2a3a40225515a050226dc2c26
henry <henryborchers@yahoo.com>
2020-05-07 16:49:11 -0500

    implimenting AbsEnumView requires  connector(self) to implimented

--- tyko/views/cassette_tape.py
+++ tyko/views/cassette_tape.py
@@ -1,39 +1,53 @@
+import abc
 from flask import views, request, jsonify, make_response
 from tyko.data_provider import CassetteTypeConnector, \
     CassetteTapeTypeConnector, \
-    CassetteTapeThicknessConnector
+    CassetteTapeThicknessConnector, \
+    AbsDataProviderConnector
 
 
 class AbsEnumView(views.MethodView):
+
+    def __init__(self, provider):
+        super().__init__()
+        self._provider = provider
+
     def get(self):
         requested_id = request.args.get("id")
-        results = self._connector.get(id=requested_id, serialize=True)
+        results = self.connector.get(id=requested_id, serialize=True)
         if results is None:
             return make_response("Unable to find requested cassette types",
                                  404)
         return jsonify(results)
 
+    @property
+    @abc.abstractmethod
+    def connector(self) -> AbsDataProviderConnector:
+        """Returns the proper AbsDataProviderConnector needed to connect to the
+        enum tables"""
+
 
 class CassetteTapeFormatTypesAPI(AbsEnumView):
-    def __init__(self, provider):
-        self._provider = provider
-        self._connector = CassetteTypeConnector(provider.db_session_maker)
+    @property
+    def connector(self):
+        return CassetteTypeConnector(self._provider.db_session_maker)
 
     def post(self):
         args = request.get_json()
         name = args["name"]
-        res = self._connector.create(name=name)
+        res = self.connector.create(name=name)
         return jsonify(res)
 
 
 class CassetteTapeTapeTypesAPI(AbsEnumView):
-    def __init__(self, provider):
-        self._provider = provider
-        self._connector = CassetteTapeTypeConnector(provider.db_session_maker)
+
+    @property
+    def connector(self):
+        return CassetteTapeTypeConnector(self._provider.db_session_maker)
 
 
 class CassetteTapeThicknessAPI(AbsEnumView):
-    def __init__(self, provider):
-        self._provider = provider
-        self._connector = \
-            CassetteTapeThicknessConnector(provider.db_session_maker)
+
+    @property
+    def connector(self):
+        return CassetteTapeThicknessConnector(self._provider.db_session_maker)
