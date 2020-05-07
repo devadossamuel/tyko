9190b2d4af87baa649a225b06413ce281d57ee70
henry <henryborchers@yahoo.com>
2020-05-07 17:23:18 -0500

    whitespace for js

--- tyko/static/js/init_site.mjs
+++ tyko/static/js/init_site.mjs
@@ -1,4 +1,4 @@
-export function initDb(init_url, restart_url){
+export function initDb(init_url, restart_url) {
   const xhr = new XMLHttpRequest();
   xhr.withCredentials = false;
 
@@ -9,38 +9,37 @@ export function initDb(init_url, restart_url){
       }
       if (xhr.status >= 200 && xhr.status < 300) {
         let data = {
-            'status': xhr.status,
-            'responseText': xhr.responseText,
-            'statusText': xhr.statusText,
-          }
+          'status': xhr.status,
+          'responseText': xhr.responseText,
+          'statusText': xhr.statusText,
+        };
 
-        if (xhr.getResponseHeader("Content-Type") === "application/json"){
-          data['response'] =  JSON.parse(xhr.response);
-        }  else {
-          data['response'] =  xhr.response
+        if (xhr.getResponseHeader('Content-Type') === 'application/json') {
+          data['response'] = JSON.parse(xhr.response);
+        } else {
+          data['response'] = xhr.response;
         }
 
         resolve(data);
       } else {
-        reject(xhr.statusText)
+        reject(xhr.statusText);
       }
 
-    }
-    xhr.open("post", init_url, true);
+    };
+    xhr.open('post', init_url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send();
-  }))
-
+  }));
 
   initDataRequest.then(function(res) {
-    const response_data = res['response']
-    console.log("SUCCESS");
-    return restart_url
+    const response_data = res['response'];
+    console.log('SUCCESS');
+    return restart_url;
   }).then(function(res) {
-      console.log("running " + res)
+    console.log('running ' + res);
   }).catch(function(reason) {
-    console.error("Failed " + reason);
-  })
+    console.error('Failed ' + reason);
+  });
 
   // location.reload()
 }
