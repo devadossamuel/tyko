8201ebf74092ffd812b7ab4576cb1d9f731c4839
henry <henryborchers@yahoo.com>
2020-05-08 09:47:31 -0500

    jsdoc for updateOptions

--- tyko/static/js/load_format_options.mjs
+++ tyko/static/js/load_format_options.mjs
@@ -1,6 +1,18 @@
 import {requests} from './request.js';
 import {items} from './api.js';
 
+/**
+ * @callback displayCallback
+ * @param {Object} data
+ * @return {string} Formatted text to be displayed
+ */
+
+/**
+ * Get and load enum option for select
+ * @param {string} url
+ * @param {string} optionId
+ * @param {displayCallback} [displayCallback]
+ */
 function updateOptions(url, optionId, displayCallback = null) {
   if (displayCallback === null) {
 
@@ -22,81 +34,87 @@ function updateOptions(url, optionId, displayCallback = null) {
   });
 }
 
-$('.tyko-form-new-entity').each(function() {
-  const form = $(this);
-  const addUrl = $(this).data('addapiurl');
-  form.unbind('submit').bind('submit',
-      function(event) {
-        event.preventDefault();
-        const rawData = $(this).serializeArray();
-        const data = {};
-        for (let i = 0; i < rawData.length; i++) {
-          const key = rawData[i].name.split('.');
+/**
+ * Loads all the class attributes
+ */
+function loadTykoClasses() {
+  $.each($('.tyko-form-new-entity'), function(i, element) {
+    const form = $(element);
+    const addUrl = $(element).data('addapiurl');
+    form.unbind('submit').bind('submit',
+        function(event) {
+          event.preventDefault();
+          const rawData = $(element).serializeArray();
+          const data = {};
+          for (let i = 0; i < rawData.length; i++) {
+            const key = rawData[i].name.split('.');
 
-          if (key.length === 1) {
-            data[key[0]] = rawData[i].value;
-            continue;
-          }
+            if (key.length === 1) {
+              data[key[0]] = rawData[i].value;
+              continue;
+            }
 
-          if (key.length === 2) {
-            if (data.hasOwnProperty(key[0])) {
-              data[key[0]][key[1]] = rawData[i].value;
-            } else {
-              const o = {};
-              o[key[1]] = rawData[i].value;
-              data[key[0]] = o;
+            if (key.length === 2) {
+              if (data.hasOwnProperty(key[0])) {
+                data[key[0]][key[1]] = rawData[i].value;
+              } else {
+                const o = {};
+                o[key[1]] = rawData[i].value;
+                data[key[0]] = o;
+              }
             }
           }
+          items.addItem(addUrl, data).then(function() {
+            location.reload();
+          }).catch(function(reason) {
+            const alertBox = $('#submitResultAlert');
+            let responsesMessage =
+                '<div class="alert alert-danger alert-dismissible" ' +
+                'role="alert" id="submitResultAlert">\n' +
+                '<strong id="errorMessage">';
+
+            responsesMessage += reason.statusText;
+            responsesMessage += '</strong>';
+            responsesMessage +=
+                '    <button type="button" class="close" ' +
+                'data-dismiss="alert" aria-label="Close">\n' +
+                '        <span aria-hidden="true">&times;</span>\n' +
+                '</div>';
+            alertBox.html(responsesMessage);
+            console.error(reason.responseText);
+          });
+          return false;
+        },
+    );
+  });
+
+  $.each($('.tyko-input-enum-api-options'), function(i, element) {
+    const select = $(element);
+    const apiUrl = $(element).data('enumapi');
+    requests.get(apiUrl).then((data) => {
+      const jsonData = JSON.parse(data);
+      jsonData.forEach(function(it) {
+        select.append(`<option value="${it['id']}">${it['name']}</option>`);
+      });
+    }).catch((resp) => {
+      alert(`Failed to load data from ${apiUrl}`);
+    });
+  });
+
+  $.each($('.tyko-input-fulldate'), function(i, element) {
+    $(element).datepicker({
+      format: 'mm-dd-yyyy',
+      uiLibrary: 'bootstrap4',
+    });
+  });
+
+  updateOptions(CASSETTE_TAPE_THICKNESS_URL, 'cassetteTapeThicknessInput',
+      function(item) {
+        if (item['unit'] === null) {
+          return item['value'];
         }
-        items.addItem(addUrl, data).then(function() {
-          location.reload();
-        }).catch(function(reason) {
-          const alertBox = $('#submitResultAlert');
-          let responsesMessage =
-              '<div class="alert alert-danger alert-dismissible" ' +
-              'role="alert" id="submitResultAlert">\n' +
-              '<strong id="errorMessage">';
+        return `${item['value']} ${item['unit']}`;
+      });
+}
 
-          responsesMessage += reason.statusText;
-          responsesMessage += '</strong>';
-          responsesMessage +=
-              '    <button type="button" class="close" data-dismiss="alert" ' +
-              'aria-label="Close">\n' +
-              '        <span aria-hidden="true">&times;</span>\n' +
-              '    </button>\n' +
-              '</div>';
-          alertBox.html(responsesMessage);
-          console.error(reason.responseText);
-        });
-        return false;
-      },
-  );
-});
-
-$('.tyko-input-enum-api-options').each(function(index) {
-  const select = $(this);
-  const apiUrl = $(this).data('enumapi');
-  requests.get(apiUrl).then((data) => {
-    const jsonData = JSON.parse(data);
-    jsonData.forEach(function(it) {
-      select.append(`<option value="${it['id']}">${it['name']}</option>`);
-    });
-  }).catch((resp) => {
-    alert('failed to get cassette_tape_types');
-  });
-});
-
-$('.tyko-input-fulldate').each(function() {
-  $(this).datepicker({
-    format: 'mm-dd-yyyy',
-    uiLibrary: 'bootstrap4',
-  });
-});
-
-updateOptions(CASSETTE_TAPE_THICKNESS_URL, 'cassetteTapeThicknessInput',
-    function(item) {
-      if (item['unit'] === null) {
-        return item['value'];
-      }
-      return `${item['value']} ${item['unit']}`;
-    });
+loadTykoClasses();
