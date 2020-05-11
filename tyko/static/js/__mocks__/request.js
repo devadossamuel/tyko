dc57a47e0335eac01aebab54e5a3e2caa41c35db
henry <henryborchers@yahoo.com>
2020-05-11 15:14:46 -0500

    TYKO-63 Audio Cassette Item details - mock format data

--- tyko/static/js/__mocks__/request.js
+++ tyko/static/js/__mocks__/request.js
@@ -15,11 +15,45 @@ const sampleNotesData = `
     }
   ]
 }`;
+const sampleFormatData = `[
+  {
+    "format_types_id": 1,
+    "name": "audio video"
+  },
+  {
+    "format_types_id": 2,
+    "name": "audio"
+  },
+  {
+    "format_types_id": 3,
+    "name": "video"
+  },
+  {
+    "format_types_id": 4,
+    "name": "open reel"
+  },
+  {
+    "format_types_id": 5,
+    "name": "grooved disc"
+  },
+  {
+    "format_types_id": 6,
+    "name": "film"
+  },
+  {
+    "format_types_id": 7,
+    "name": "casette tape"
+  }
+]`;
 export const requests = {
   'get': (apiPath) => {
-    return new Promise(((resolve, reject) => {
-      resolve(sampleNotesData);
-    }));
+    return new Promise((resolve, reject) => {
+      if (apiPath === '/api/formats') {
+        resolve(sampleFormatData);
+      } else {
+        resolve(sampleNotesData);
+      }
+    });
   },
   'post': (apiPath, data) => {
     return new Promise(((resolve, reject) => {
